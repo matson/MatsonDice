6e5ce36361361cf8a1ec11b8c4096d94aacb34da
Madison <madison.k.adams@gmail.com>
2023-04-24 14:29:46 -0400

    beginning files

---
+++ MatsonDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatsonDice
+//
+//  Created by Tracy Adams on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
