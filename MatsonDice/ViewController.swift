5945ec5f7d29c89acc9a04e8fea70bc0f6c7c054
Madison <madison.k.adams@gmail.com>
2023-04-25 21:41:26 -0400

    more logic

--- MatsonDice/ViewController.swift
+++ MatsonDice/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImage3: UIImageView!
     
+    @IBOutlet weak var sumDice: UILabel!
     
     override func viewDidLoad() {
         
@@ -27,14 +28,12 @@ class ViewController: UIViewController {
     
     @IBAction func rollButton(_ sender: UIButton) {
         
-        
-        
         var sum1 = 0;
         var sum2 = 0;
         var sum3 = 0;
         var totalSum = 0;
         
-        var diceArray : Array<UIImage> = [UIImage(named: "dice1")!,
+        let diceArray : Array<UIImage> = [UIImage(named: "dice1")!,
                                           UIImage(named: "dice2")!,
                                           UIImage(named: "dice3")!,
                                           UIImage(named: "dice4")!,
@@ -43,76 +42,60 @@ class ViewController: UIViewController {
                                                                 ]
         
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
-        if(diceImageView1.image == diceArray[0]){
-            sum1+=1; //one
-        }
-        else if(diceImageView1.image == diceArray[1]){
-            sum1+=2; //two
-        }
-        else if(diceImageView1.image == diceArray[2]){
-            sum1+=3; //three
-        }
-        else if(diceImageView1.image == diceArray[3]){
-            sum1+=4; //four
-        }
-        else if(diceImageView1.image == diceArray[4]){
-            sum1+=5; //five
-        }
-        else if(diceImageView1.image == diceArray[5]){
-            sum1+=6; //six
-        }
+        let dice1 = diceImageView1.image!
+        sum1 = findValue(array: diceArray, image: dice1)
+        let dice2 = diceImageView2.image!
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        if(diceImageView1.image == diceArray[0]){
-            sum2+=1; //one
-        }
-        else if(diceImageView2.image == diceArray[1]){
-            sum2+=2; //two
-        }
-        else if(diceImageView2.image == diceArray[2]){
-            sum2+=3; //three
-        }
-        else if(diceImageView2.image == diceArray[3]){
-            sum2+=4; //four
-        }
-        else if(diceImageView2.image == diceArray[4]){
-            sum2+=5; //five
-        }
-        else if(diceImageView2.image == diceArray[5]){
-            sum2+=6; //six
-        }
-        
-        
+        sum2 = findValue(array: diceArray, image: dice2)
+        let dice3 = diceImage3.image!
         diceImage3.image = diceArray[Int.random(in: 0...5)]
-        if(diceImage3.image == diceArray[0]){
-            sum3+=1; //one
-        }
-        else if(diceImage3.image == diceArray[1]){
-            sum3+=2; //two
-        }
-        else if(diceImage3.image == diceArray[2]){
-            sum3+=3; //three
-        }
-        else if(diceImage3.image == diceArray[3]){
-            sum3+=4; //four
-        }
-        else if(diceImage3.image == diceArray[4]){
-            sum3+=5; //five
-        }
-        else if(diceImage3.image == diceArray[5]){
-            sum3+=6; //six
-        }
+        sum3 = findValue(array: diceArray, image: dice3)
+        
         print("this is sum 1..\(sum1)")
         print("this is sum 2..\(sum2)")
         print("this is sum 3..\(sum3)")
         totalSum = sum1+sum2+sum3
-        print(totalSum)
+        
+        sumDice.text = String(totalSum)
     
         
     }
     
     //define function here:
+    //logic for comparing dice values
+   
+    func findValue(array: Array<UIImage>, image: UIImage) -> Int {
+        //takes in
+        var value = 0;
+        if(image === array[0]){
+            value = 1; //one
+        }
+        else if(image === array[1]){
+            value = 2; //two
+        }
+        else if(image === array[2]){
+            value = 3; //three
+        }
+        else if(image === array[3]){
+            value = 4; //four
+        }
+        else if(image === array[4]){
+            value = 5; //five
+        }
+        else if(image === array[5]){
+            value = 6; //six
+        }
+        return value
+    }
     
     
 
 }
 
+/*
+ 
+ Things that have to be changed:
+ Element contraints.
+ Logic is a little questionable.  Sum is not always correct..
+ 
+ */
