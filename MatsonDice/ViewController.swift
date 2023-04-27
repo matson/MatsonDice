2ec331b9157d88918117fc1fce5e46c9e5606802
Madison <madison.k.adams@gmail.com>
2023-04-27 16:37:28 -0400

    fixed logic

--- MatsonDice/ViewController.swift
+++ MatsonDice/ViewController.swift
@@ -29,27 +29,31 @@ class ViewController: UIViewController {
     @IBAction func rollButton(_ sender: UIButton) {
         
         
-        let diceArray : Array<UIImage> = [UIImage(named: "dice1")!,
-                                          UIImage(named: "dice2")!,
-                                          UIImage(named: "dice3")!,
-                                          UIImage(named: "dice4")!,
-                                          UIImage(named: "dice5")!,
-                                          UIImage(named: "dice6")!
-                                                                ]
+        //Maybe define a dictionary instead?
+        let diceDict = [UIImage(named: "dice1")! : 1,
+                        UIImage(named: "dice2")! : 2,
+                        UIImage(named: "dice3")!: 3,
+                        UIImage(named: "dice4")!: 4,
+                        UIImage(named: "dice5")!: 5,
+                        UIImage(named: "dice6")!: 6
         
-        diceImageView1.image = diceArray[Int.random(in: 0...5)]
-        let dice1 = diceImageView1.image!
-        let value1 = findValue(array: diceArray, image: dice1)
-        let dice2 = diceImageView2.image!
-        diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        let value2 = findValue(array: diceArray, image: dice2)
-        let dice3 = diceImage3.image!
-        diceImage3.image = diceArray[Int.random(in: 0...5)]
-        let value3 = findValue(array: diceArray, image: dice3)
+        ]
         
-        print("this is value for dice1..\(value1)")
-        print("this is value for dice2..\(value2)")
-        print("this is value for dice3..\(value3)")
+        let random1 = diceDict.randomElement()!
+        let random2 = diceDict.randomElement()!
+        let random3 = diceDict.randomElement()!
+       
+        
+        diceImageView1.image = random1.key
+        
+        diceImageView2.image = random2.key
+
+        diceImage3.image = random3.key
+        
+        let value1 = random1.value
+        let value2 = random2.value
+        let value3 = random3.value
+    
         let totalSum = value1+value2+value3
         
         sumDice.text = String(totalSum)
@@ -60,37 +64,37 @@ class ViewController: UIViewController {
     //define function here:
     //logic for comparing dice values
    
-    func findValue(array: Array<UIImage>, image: UIImage) -> Int {
-        //takes in
-        var value = Int();
-        if(image === array[0]){
-            value = 1; //one
-            print("was 1 here")
-        }
-        else if(image === array[1]){
-            value = 2; //two
-            print("was 2 here")
-        }
-        else if(image === array[2]){
-            value = 3; //three
-            print("was 3 here")
-        }
-        else if(image === array[3]){
-            value = 4; //four
-            print("was 4 here")
-        }
-        else if(image === array[4]){
-            value = 5; //five
-            print("was 5 here")
-        }
-        else if(image === array[5]){
-            value = 6; //six
-            print("was 6 here")
-        }
-        print(value)
-        return value
-    }
-    
+//    func findValue(array: Array<UIImage>, image: UIImage) -> Int {
+//        //takes in
+//        var value = Int();
+//        if(image === array[0]){
+//            value = 1; //one
+//            print("was 1 here")
+//        }
+//        else if(image === array[1]){
+//            value = 2; //two
+//            print("was 2 here")
+//        }
+//        else if(image === array[2]){
+//            value = 3; //three
+//            print("was 3 here")
+//        }
+//        else if(image === array[3]){
+//            value = 4; //four
+//            print("was 4 here")
+//        }
+//        else if(image === array[4]){
+//            value = 5; //five
+//            print("was 5 here")
+//        }
+//        else if(image === array[5]){
+//            value = 6; //six
+//            print("was 6 here")
+//        }
+//        print(value)
+//        return value
+//    }
+//
     
 
 }
